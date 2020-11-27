47f929388dc1c8b72e418545c6b01a3929e37d7d
Danny Lionel <32670499+DannyLionel@users.noreply.github.com>
2020-11-27 13:05:59 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,9 +12,9 @@ export default class Navbar extends Component {
 https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
 https://www.iconfinder.com/Makoto_msk */}
-        <Link to="/">
+        <a href="https://condescending-ride-2b8940.netlify.app/">
           <img src={logo} alt="store" className="navbar-brand"></img>
-        </Link>
+        </a>
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
             <Link to="/" className="nav-link">
